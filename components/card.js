34b338d8265508c068d5338c8c81fa72dcbe4287
Rohan Jacob <rohan.rj120@gmail.com>
2023-06-12 10:27:41 +0530

    card table formatting and current period

--- components/card.js
+++ components/card.js
@@ -13,23 +13,76 @@ export default function Card({ name, timetable }) {
   const now = new Date(Date.now());
   const timetableToday = timetable[(now.getDay() - 1 + 7) % 7];
 
-  const [expanded, setExpanded] = useState(true);
+  var color = "to-be-busy";
+  var period = {
+    title: "",
+    venue: "",
+    start: "",
+    end: "",
+  };
+
+  for (
+    var currentClass = 0;
+    currentClass < timetableToday.length;
+    currentClass++
+  ) {
+    period = timetableToday[currentClass];
+    let start = new Date(
+      now.getFullYear(),
+      now.getMonth(),
+      now.getDate(),
+      period.start.substring(11, 13),
+      period.start.substring(14, 16),
+      0
+    );
+    let end = new Date(
+      now.getFullYear(),
+      now.getMonth(),
+      now.getDate(),
+      period.end.substring(11, 13),
+      period.end.substring(14, 16),
+      0
+    );
+    if (end >= now) {
+      if (start <= now) {
+        color = "busy";
+      }
+      break;
+    }
+  }
+
+  if (currentClass == timetableToday.length) {
+    color = "free";
+  }
+
+  // console.log(name, i, color);
+
+  const [expanded, setExpanded] = useState(false);
   var middle;
   if (expanded) {
     middle = (
       <div className={style.middle}>
         <table>
           <tbody>
-            {timetableToday.map((period) => (
-              <tr key={uuidv4()}>
-                <td>
-                  {period.start.substring(11, 16)}-
-                  {period.end.substring(11, 16)}
-                </td>
-                <td>{period.venue}</td>
-                <td>{period.title}</td>
-              </tr>
-            ))}
+            {timetableToday.map((period, i) => {
+              let style = {};
+              if (i < currentClass) {
+                style = { textDecoration: "line-through", color: "gray" };
+              } else if (i == currentClass) {
+                style = { background: `var(--${color})`, color: "#000000c8" };
+              }
+              console.log(name, currentClass, style);
+              return (
+                <tr key={uuidv4()} style={style}>
+                  <td>
+                    {period.start.substring(11, 16)}-
+                    {period.end.substring(11, 16)}
+                  </td>
+                  <td>{period.venue}</td>
+                  <td>{period.title}</td>
+                </tr>
+              );
+            })}
           </tbody>
         </table>
       </div>
@@ -60,12 +113,19 @@ export default function Card({ name, timetable }) {
           </a>
           <div className={style.right}>
             <div className={style.subject}>
-              <div className={style.subName}></div>
+              <div className={style.subName}>{period.title}</div>
             </div>
             <div className={style.timeloc}>
               <FontAwesomeIcon icon={faClock} />
-              <div className={style.time}></div>~
-              <div className={style.location}></div>
+              <div className={style.time}>
+                {period.title
+                  ? `${period.start.substring(11, 16)}-${period.end.substring(
+                      11,
+                      16
+                    )}`
+                  : ""}
+              </div>
+              ~<div className={style.location}>{period.venue}</div>
               <FontAwesomeIcon icon={faLocationCrosshairs} />
             </div>
           </div>
