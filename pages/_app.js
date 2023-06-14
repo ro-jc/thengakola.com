af812bcd8c73d8973318d779113baf9d299f2a2b
Rohan Jacob <rohan.rj120@gmail.com>
2023-06-14 20:10:26 +0530

    add analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,15 @@
 import "../styles/globals.css";
+import { Analytics } from "@vercel/analytics/react";
 
 import { config } from "@fortawesome/fontawesome-svg-core";
 import "@fortawesome/fontawesome-svg-core/styles.css";
 config.autoAddCss = false;
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      <Analytics />
+    </>
+  );
 }
