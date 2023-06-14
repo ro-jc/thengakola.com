de8354d8b56395450eaea6794ccc304bdbf394b0
Rohan Jacob <rohan.rj120@gmail.com>
2023-06-14 23:20:17 +0530

    fix meta params

--- components/base.js
+++ components/base.js
@@ -6,8 +6,8 @@ export default function Base({ children, offset }) {
   return (
     <>
       <Head>
-        <meta charset="UTF-8" />
-        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta charSet="UTF-8" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
         <meta
           name="viewport"
           content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"
